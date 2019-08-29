2a412d666d19756b428d74d8b33a7e75b57ba8a4
Dom Painter <dominickpainter@gmail.com>
2019-08-29 18:38:55 +1000

    Reduce data by years and begin aggregating years data

--- src/parse-weather-data.js
+++ src/parse-weather-data.js
@@ -1,3 +1,28 @@
 module.exports = weatherData => {
-    console.log(weatherData)   
+    return weatherData.reduce((acc, curr) => {
+        if (!acc[curr.Year]) {
+            // Start object structure for a year entry
+            acc[curr.Year] = {
+                Year: curr.Year,
+                FirstRecordedDate: 0,
+                LastRecordedDate: 0,
+                TotalRainfall: 0,
+                AverageDailyRainfall: 0,
+                DaysWithNoRainfall: 0,
+                DaysWithRainfall: 0,
+            }
+        }
+
+        // Sum up the TotalRainfall value for a year
+        acc[curr.Year].TotalRainfall += Number(curr['Rainfall amount (millimetres)'])
+
+        // Increase daysWith and daysWithNo depending on level of rainfall
+        if (Number(curr['Rainfall amount (millimetres)'])) {
+            acc[curr.Year].DaysWithRainfall++ 
+        } else {
+            acc[curr.Year].DaysWithNoRainfall++
+        }
+
+        return acc
+    }, {})
 }

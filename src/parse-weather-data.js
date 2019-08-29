78516ff70ced6c578e108b999eef8aee294e8ace
Dom Painter <dominickpainter@gmail.com>
2019-08-29 19:26:57 +1000

    Add monthly aggregate calculations

--- src/parse-weather-data.js
+++ src/parse-weather-data.js
@@ -1,5 +1,17 @@
-module.exports = weatherData => {
-    return weatherData.reduce((acc, curr) => {
+const calendarMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+const getMonthObject = monthName => ({
+    Month: monthName,
+    FirstRecordedDate: '',
+    LastRecordedDate: '',
+    TotalRainfall: 0,
+    AverageDailyRainfall: 0,
+    DaysWithNoRainfall: 0,
+    DaysWithRainfall: 0,
+})
+
+module.exports = weatherData => 
+    weatherData.reduce((acc, curr) => {
         const {Year: year, Month: month, Day: day, 'Rainfall amount (millimetres)': rainfall} = curr
         const rainfallAmount = Number(rainfall)
 		
@@ -17,21 +29,35 @@ module.exports = weatherData => {
             }
         }
 
+        // If the current year doesnt already have a Month object for `month`, create one using the shape
+        const currentMonthName = calendarMonths[Number(month)]
+        const foundMonth = acc[year].MonthlyAggregates.find(({Month}) => Month === currentMonthName)
+        const monthAggregate = foundMonth || getMonthObject(currentMonthName)
+
         // Sum up the TotalRainfall value for a year
         acc[year].TotalRainfall += rainfallAmount
+        monthAggregate.TotalRainfall += rainfallAmount // update month rainfall total
 
         // Increase daysWith and daysWithNo depending on level of rainfall
         if (rainfallAmount) {
             acc[year].DaysWithRainfall++ 
+            monthAggregate.DaysWithRainfall++
         } else {
             acc[year].DaysWithNoRainfall++
+            monthAggregate.DaysWithNoRainfall++
         }
 
-        const totalDays = acc[year].DaysWithRainfall + acc[year].DaysWithNoRainfall
+        const totalDaysInYear = acc[year].DaysWithRainfall + acc[year].DaysWithNoRainfall
+        const totalDaysInMonth = monthAggregate.DaysWithRainfall + monthAggregate.DaysWithNoRainfall
 
         // Update the average daily rainfall
-        acc[year].AverageDailyRainfall = acc[year].TotalRainfall / totalDays 
+        acc[year].AverageDailyRainfall = acc[year].TotalRainfall / totalDaysInYear
+        monthAggregate.AverageDailyRainfall = monthAggregate.TotalRainfall / totalDaysInMonth
 
+        // Month was not present in current data set, so add it
+        if (!foundMonth) {
+            acc[year].MonthlyAggregates.push(monthAggregate)
+        }
+        
         return acc
     }, {})
-}
